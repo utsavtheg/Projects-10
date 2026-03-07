544f5e54612fbd0859bf120f1abe3df16a23246d
Utsav Gupta <utsavgupta309@gmail.com>
2026-03-07 21:39:38 +0530

    add

---
+++ color.js
@@ -0,0 +1,56 @@
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = (Math.floor(Math.random() * 16));
+    color += letters[randomIndex];
+  }
+  return color;
+}
+
+function createCard(color) {
+  const card = document.createElement("div");
+  card.setAttribute("class", "color-card");
+  card.style.backgroundColor = color;
+
+  const code = document.createElement("span");
+  code.setAttribute("class", "color-code");
+  code.textContent = color;
+
+  card.append(code);
+
+
+
+  return card;
+
+
+}
+function generatePalette() {
+  const palette = document.querySelector("#colorPalette");
+  palette.innerHTML = '';
+
+  for (let i = 0; i < 5; i++) {
+    const randomColor = getRandomColor();
+    const card = createCard(randomColor);
+    palette.append(card);
+  }
+
+
+}
+
+const generateBtn = document.querySelector("#generateBtn");
+
+generateBtn.addEventListener("click", generatePalette);
+
+generatePalette();
+
+
+
+
+
+
+
+
+
+
